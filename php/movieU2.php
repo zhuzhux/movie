1086a786b58dbcd9292b4c37fdd4a26e6e6deba9
786507017@qq.com <786507017@qq.com>
2017-07-05 15:34:02 +0800

    更换了解析，增加了多个选择
    更换了解析

--- php/movieU2.php
+++ php/movieU2.php
@@ -1,8 +1,8 @@
 <?php
 	//这个返回的是播放页面的地址，还不是真实地址
-//	'http://www.yingshidaquan.cc/play/DQ238234-2-1.html'
+	$url = 'http://www.yingshidaquan.cc/play/DQ238234-2-1.html';
 //$url = 'http://m.yingshidaquan.cc/play/DQ237238-3-1.html';  //这儿填页面地址
-$url = $_POST['aHref'];  //这儿填页面地址
+//$url = $_POST['aHref'];  //这儿填页面地址
 $info=file_get_contents($url);
 $result = array();
 if(preg_match_all('/<script language="javascript">var ff_urls=\'([\w\W]*)\'\;<\/script>/iU', $info, $result)){
